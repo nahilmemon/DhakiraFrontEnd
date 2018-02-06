e36f9f3b7d703439a3a4ca1cbfd5410b6e91e4fd
nahilmemon <nam432@nyu.edu>
2018-02-07 01:27:22 +0400

    Experimenting with modals on the manage-members page. Fixed bug where modal appears faded.

--- js/script-main.js
+++ js/script-main.js
@@ -6,9 +6,30 @@ $(document).ready(function(){
   $(".carousel-img").height(carouselImageHeight);
 
   // Semantic UI Dropdown
+  // This allows the filtering dropdowns on the hompage to work.
   $('.ui.dropdown')
-  .dropdown()
-;
+    .dropdown()
+  ;
+
+  $(document).on('hidden.bs.modal', function (e) {
+    var target = $(e.target);
+    target.removeData('bs.modal')
+    .find(".clearable-content").html('');
+  });
+
+  // Bootstrap Modal
+  // This allows the modal to work on the homepage.
+  $('#storyModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    var description = button.data('description')
+    // Update the modal's content using jQuery.
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+    modal.find('.modal-body textarea').val(description)
+  })
+
 
 })
 
