8e5526e6f94afe85db2903ed27b3e8da6f44e54c
nahilmemon <nam432@nyu.edu>
2018-02-09 15:16:40 +0400

    Fixed bug where the carousel would take up more space the width of the page. Also decreased padding on the carousel caption for smaller devices.

--- js/script-main.js
+++ js/script-main.js
@@ -2,8 +2,24 @@ $(document).ready(function(){
   // Make sure the story card tiles are always the same dimensions
   var panelHeight = $(".panel-body-height").width()*0.67;
   $(".panel-body-height").height(panelHeight);
-  var carouselImageHeight = $("div.carousel-inner > div.item.active").width()*0.67;
-  $(".carousel-img").height(carouselImageHeight);
+
+  // If in landscape mode, then make the carousel take up the whole screen's height and width
+  if (window.innerHeight < window.innerWidth) {
+    var carouselContainerHeight = window.innerHeight - 70 - 50; // 70 = navbar height, 50 = footer height
+    $("#myCarousel").height(carouselContainerHeight);
+    var carouselContainerWidth = $("section#featured.jumbotron").width();
+    $("section#featured > div.row").width(carouselContainerWidth);
+    $(".carousel-img").height(carouselContainerHeight);
+    $(".carousel-img").width(carouselContainerWidth);
+  } else { // else just take up the whole width, and 67% of the height of the width
+    var carouselContainerWidth = $("section#featured.jumbotron").width();
+    var carouselContainerHeight = carouselContainerWidth * 0.67;
+    $("#myCarousel").height(carouselContainerHeight);
+    $("section#featured > div.row").width(carouselContainerWidth);
+    $(".carousel-img").height(carouselContainerHeight);
+    $(".carousel-img").width(carouselContainerWidth);
+  }
+
   // Change the font-size of the sound icon in the story panel
   // according to the size of the story panel
   var soundIconFontSize = panelHeight*0.8;
@@ -178,8 +194,24 @@ $(window).resize(function(){
   // Make sure the story card tiles are always the same dimensions
   var panelHeight = $(".panel-body-height").width()*0.67;
   $(".panel-body-height").height(panelHeight);
-  var carouselImageHeight = $("div.carousel-inner > div.item.active").width()*0.67;
-  $(".carousel-img").height(carouselImageHeight);
+
+  // If in landscape mode, then make the carousel take up the whole screen's height and width
+  if (window.innerHeight < window.innerWidth) {
+    var carouselContainerHeight = window.innerHeight - 70 - 50; // 70 = navbar height, 50 = footer height
+    $("#myCarousel").height(carouselContainerHeight);
+    var carouselContainerWidth = $("section#featured.jumbotron").width();
+    $("section#featured > div.row").width(carouselContainerWidth);
+    $(".carousel-img").height(carouselContainerHeight);
+    $(".carousel-img").width(carouselContainerWidth);
+  } else { // else just take up the whole width, and 67% of the height of the width
+    var carouselContainerWidth = $("section#featured.jumbotron").width();
+    var carouselContainerHeight = carouselContainerWidth * 0.67;
+    $("#myCarousel").height(carouselContainerHeight);
+    $("section#featured > div.row").width(carouselContainerWidth);
+    $(".carousel-img").height(carouselContainerHeight);
+    $(".carousel-img").width(carouselContainerWidth);
+  }
+
   // Change the font-size of the sound and film icons in the story panel
   // according to the size of the story panel
   var soundIconFontSize = panelHeight*0.8;
